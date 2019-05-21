b7d2eb672f5d9ce18d20ccad07b27069aaab30b4
Toh Wen Xiang <tohwenxiang@gmail.com>
2019-05-21 12:41:47 +0800

    Before attempting to modify death and revive system

--- Assets/Scripts/Character/CharacterSpawner.cs
+++ Assets/Scripts/Character/CharacterSpawner.cs
@@ -5,26 +5,6 @@ using Outbreak;
 
 public class CharacterSpawner : MonoBehaviour
 {
-    
-
-    private static CharacterSpawner instance = null;
-
-    private void Awake()
-    {
-        if (instance != null)
-        {
-            if (instance != this)
-            {
-                Destroy(gameObject);
-                Debug.LogError("There must be only one player spawn in the game");
-            }
-        }
-        else
-        {
-            instance = this;
-        }
-    }
-
     // Start is called before the first frame update
     void Start()
     {
